c1378bcf6687556e2b9682890c7a536130faa3b5
Alonso R <luis.alonso.16@hotmail.com>
2020-04-02 19:45:10 -0700

    Add required dependecies

--- src/book-service/cmd/alexandria-server/main.go
+++ src/book-service/cmd/alexandria-server/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "log"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
 
 func main() {
-	log.Print("Hello from alexandria server")
+	engine := gin.Default()
+
+	engine.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, &gin.H{
+			"message": "Hello from Book HTTP API",
+		})
+	})
+
+	server := http.Server{
+		Addr:              ":8080",
+		Handler:           engine,
+		TLSConfig:         nil,
+		ReadTimeout:       15 * time.Second,
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      15 * time.Second,
+		IdleTimeout:       30 * time.Second,
+		MaxHeaderBytes:    2048,
+		TLSNextProto:      nil,
+		ConnState:         nil,
+		ErrorLog:          nil,
+		BaseContext:       nil,
+		ConnContext:       nil,
+	}
+
+	err := server.ListenAndServe()
+	defer func() {
+		err := server.Shutdown(context.Background())
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	if err != nil {
+		panic(err)
+	}
 }
--- src/book-service/go.mod
+++ src/book-service/go.mod
@@ -1,3 +1,10 @@
 module github.com/maestre3d/alexandria/src/book-service
 
 go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.6.2 // indirect
+	github.com/google/wire v0.4.0 // indirect
+	github.com/sony/gobreaker v0.4.1 // indirect
+	gocloud.dev v0.19.0 // indirect
+)
