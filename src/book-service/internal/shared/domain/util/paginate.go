07aa4b1aad52b958066b105a5d84f26ebdec53db
Alonso R <luis.alonso.16@hotmail.com>
2020-04-03 20:56:15 -0700

    Fix paginate algorithm description

--- src/book-service/internal/shared/domain/util/paginate.go
+++ src/book-service/internal/shared/domain/util/paginate.go
@@ -3,7 +3,7 @@ package util
 func GetIndex(page, limit int64) int64 {
 	// Index-from-limit algorithm formula
 	// f(x)= w-x
-	// w (omega) = x*n
-	// where x = limit and n = page
+	// w (omega) = xy
+	// where x = limit and y = page
 	return limit*page - limit
 }
