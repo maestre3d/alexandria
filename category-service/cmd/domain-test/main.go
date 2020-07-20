ef23af10a84a07e0a41b7d244805b4293aa32638
A Ruiz <luis.alonso.16@hotmail.com>
2020-07-19 22:14:46 -0600

    Add category internal testing

--- category-service/cmd/domain-test/main.go
+++ category-service/cmd/domain-test/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/alexandria-oss/core/config"
 	"github.com/alexandria-oss/core/logger"
 	"github.com/alexandria-oss/core/persistence"
+	"github.com/maestre3d/alexandria/category-service/internal/domain"
 	"github.com/maestre3d/alexandria/category-service/internal/infrastructure"
 	"github.com/maestre3d/alexandria/category-service/internal/interactor"
 )
@@ -29,21 +30,36 @@ func main() {
 	eventBus := infrastructure.NewCategoryEventKafka(cfg)
 	categoryI := interactor.NewCategoryUseCase(log, repo, eventBus)
 
-	/*
-		createdCat, err := categoryI.Create(ctx, "terror")
+	category := new(domain.Category)
+	catChan := make(chan *domain.Category)
+	go func() {
+		ctx1, _ := context.WithCancel(ctx)
+		category, err := categoryI.Create(ctx1, "sci-fi")
 		if err != nil {
 			panic(err)
 		}
+		catChan <- category
+	}()
+	select {
+	case category = <-catChan:
+		_ = log.Log("msg", fmt.Sprintf("%+v", category))
+		break
+	}
 
+	ctx2, _ := context.WithCancel(ctx)
+	err = categoryI.HardDelete(ctx2, category.ExternalID)
+	if err != nil {
+		_ = log.Log("err", err)
+		panic(err)
+	}
 
-	*/
-
-	createdCat, token, err := categoryI.List(ctx, "", "1", core.FilterParams{"query": "terror"})
+	ctx3, _ := context.WithCancel(ctx)
+	categories, token, err := categoryI.List(ctx3, "", "10", core.FilterParams{"query": "terror"})
 	if err != nil {
 		panic(err)
 	}
 
-	for _, cat := range createdCat {
+	for _, cat := range categories {
 		_ = log.Log("output", fmt.Sprintf("%+v", cat))
 	}
 
